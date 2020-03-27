d3add133a9dd6c696820549fa9102435e93404af
Andrii <zelenkoa@uni.coventry.ac.uk>
2020-03-27 13:50:39 +0000

    Cganged a bit

--- rrt.py
+++ rrt.py
@@ -4,25 +4,29 @@ from input.input import drones, colors
 from mpl_toolkits.mplot3d import Axes3D, axes3d
 import sys
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def main():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
 
 
-while True:
-    check = []
+    while True:
+        check = []
+        for j in range(len(drones)):
+            rrt(drone = drones[j], target=drones[j].finish)
+            check.append(drones[j].RRTfinished)
+
+        if all(check) : break
+
+
+
     for j in range(len(drones)):
-        rrt(drone = drones[j], target=drones[j].finish)
-        check.append(drones[j].RRTfinished)
-
-    if all(check) : break
+        for k in range(len(drones[j].path)):
+            ax.scatter(drones[j].path[k][0],drones[j].path[k][1], drones[j].path[k][2], color = colors[j])
 
 
 
-for j in range(len(drones)):
-    for k in range(len(drones[j].path)):
-        ax.scatter(drones[j].path[k][0],drones[j].path[k][1], drones[j].path[k][2], color = colors[j])
+    plt.show()
 
-
-
-plt.show()
+if __name__ == "__main__":
+    main()
